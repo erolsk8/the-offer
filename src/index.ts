2c5ab872513858735d1f62680ce98cbbfae67c56
Erol Zivina <erolsk8@gmail.com>
2024-01-23 18:32:53 +0100

    Implement simple footer with dummy links

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
-import './assets/styles/global.scss';
+import './assets/styles/index.scss';
 
 import './assets/scripts/header';
+
+window.addEventListener('DOMContentLoaded', () => {
+  // Lazy load footer CSS
+  import('./assets/scripts/lazy-footer');
+});
