71d1c5a83d6bed5c8b136ef144b2dbfc37962380
Erol Zivina <erolsk8@gmail.com>
2024-01-25 11:21:36 +0100

    Use dynamic imports for content below the fold

--- src/assets/scripts/lazy-footer.ts
+++
@@ -1,1 +0,0 @@
-import '../styles/lazy-footer.scss';
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,22 @@
 import './assets/styles/index.scss';
 
 import { initMenu } from './assets/scripts/header';
-import { initOffers } from './assets/scripts/offers/init';
-
-// TODO: Implement real lazy loading (e.g. on first user interaction) - if performance score drops
 
 initMenu();
-initOffers();
+
+/**
+ * Use dynamic import for content below the fold.
+ * @link https://webpack.js.org/guides/code-splitting/#dynamic-imports
+ */
+const handleDynamicImports = async (): Promise<void> => {
+  // Offers section
+  const offers = await import('./assets/scripts/offers/init');
+  offers.initOffers();
+
+  // Footer
+  import('./assets/scripts/footer');
+};
 
 window.addEventListener('DOMContentLoaded', () => {
-  // Lazy load footer CSS
-  import('./assets/scripts/lazy-footer');
+  void handleDynamicImports();
 });
